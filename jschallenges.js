d8ebd70e42e8572d9ab204e0b624dd5c55dec05c
sarah-schmid <sarah.schmid@gather.app>
2022-09-15 20:42:49 -0600

    solved the spin words challenge

--- jschallenges.js
+++ jschallenges.js
@@ -163,3 +163,34 @@ function solution(number) {
 }
 
 console.log(solution(10));
+
+/*
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples:
+
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+spinWords( "This is a test") => returns "This is a test" 
+spinWords( "This is another test" )=> returns "This is rehtona test"
+*/
+
+function spinWords(string) {
+  const splitWords = string.split(" ");
+  let reversedArry = [];
+  for (let i = 0; i < splitWords.length; i++) {
+    if (splitWords[i].length >= 5) {
+      let reversedWord = "";
+      for (let n = splitWords[i].length - 1; n > -1; n--) {
+        reversedWord += splitWords[i][n];
+      }
+      reversedArry.push(reversedWord);
+    } else {
+      reversedArry.push(splitWords[i]);
+    }
+  }
+  let sentence = reversedArry.join(" ");
+  return sentence;
+}
+
+console.log(spinWords("hello there my friend bye"));
+console.log(spinWords("Welcome"));
